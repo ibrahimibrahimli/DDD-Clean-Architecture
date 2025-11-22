60ab7547a23be8d8a35e501908c7b053ae94a24e
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-11-22 19:28:43 +0400

    Add `Money` value object class to `Domain.ValueObjects`
    Introduced a new `Money` class within the `Domain.ValueObjects` namespace.
    The class is sealed and implements the `IEquatable<Money>` interface.
    Added an `Equals(Money? other)` method, which currently throws a `NotImplementedException`.
    This sets the foundation for implementing equality logic for the `Money` value object.

---
+++ Application/Domain/ValueObjects/Money.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.ValueObjects
+{
+    public sealed class Money : IEquatable<Money>
+    {
+        public bool Equals(Money? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
