664b6680411a3788e83ed37973293b7e3e358f31
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-12-04 09:05:50 +0400

    Add BusinessRuleValidationException class
    Introduce a sealed `BusinessRuleValidationException` class to handle business rule validation errors. The class includes a constructor for custom exception messages and a `BusinessRuleCode` property for better debugging.
    
    Added an unused `using` directive for `System.Security.Cryptography.X509Certificates`, which may be a placeholder for future functionality.

---
+++ Application/Application/Common/Exceptions/BusinessRuleValidationException.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Application.Common.Exceptions
+{
+    public sealed class BusinessRuleValidationException : Exception
+    {
+        public BusinessRuleValidationException( string businessRuleCode, string message)
+            : base($"Business rule validation failed: {businessRuleCode} - {message}")
+        {
+           BusinessRuleCode = businessRuleCode;
+        }
+        public string BusinessRuleCode { get; }
+    }
+}
