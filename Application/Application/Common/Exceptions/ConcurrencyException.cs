57e793a50f1a05e6c115e3b09b9cff7e9fa658d0
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-12-05 09:11:21 +0400

    Add ConcurrencyException for handling concurrency conflicts
    Introduced a new `ConcurrencyException` class in the
    `Application.Common.Exceptions` namespace to handle concurrency
    conflicts. The class is sealed and inherits from the `Exception`
    base class. It includes two constructors: one with a default
    error message and another that accepts a custom message. This
    enhancement improves error handling and code organization.

---
+++ Application/Application/Common/Exceptions/ConcurrencyException.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Common.Exceptions
+{
+    public sealed class ConcurrencyException : Exception
+    {
+        public ConcurrencyException()
+            : base("A concurrency conflict occurred. The entity you are trying to update has been modified by another user.")
+        {
+        }
+        public ConcurrencyException(string message)
+        : base(message)
+        {
+        }
+    }
+}
