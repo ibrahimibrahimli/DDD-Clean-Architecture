6dece058e46b35c539867b6fa7efc8c6a45d7965
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-12-06 09:56:42 +0400

    Add IUnitOfWork interface to define transaction and persistence operations
    Introduced the IUnitOfWork abstraction to centralize database save operations
    and transaction handling. This interface provides methods for saving changes,
    beginning, committing, and rolling back transactions, enabling consistent
    transaction management across the application.
    
    Add UnitOfWorkBehavior to manage transactions for MediatR commands
    
    Implemented a pipeline behavior that wraps command handlers in a
    Unit of Work transaction. Queries bypass transaction handling, while
    commands automatically begin, commit, or roll back transactions depending
    on execution success. This ensures consistent data integrity across the
    application.

---
+++ Application/Application/Common/Behaviors/UnitOfWorkBehavior.cs
@@ -0,0 +1,41 @@
+﻿
+using Application.Common.Interfaces;
+using MediatR;
+
+namespace Application.Common.Behaviors
+{
+    public sealed class UnitOfWorkBehavior<TRequset, TResponse>
+        : IPipelineBehavior<TRequset, TResponse>
+        where TRequset : IRequest<TResponse>
+    {
+        readonly IUnitOfWork _unitOfWork;
+        public UnitOfWorkBehavior(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<TResponse> Handle(TRequset request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            //if is this a query dont need transaction
+            if (typeof(TRequset).Name.EndsWith("Query"))
+            {
+                return await next();
+            }
+
+            try
+            {
+                await _unitOfWork.BeginTransactionAsync(cancellationToken);
+
+                var response = await next();
+
+                await _unitOfWork.CommitTransactionAsync(cancellationToken);
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                await _unitOfWork.RollBackTransactionAsync(cancellationToken);
+                throw;
+            }
+        }
+    }
+}
---
+++ Application/Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Common.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+        Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+        Task CommitTransactionAsync(CancellationToken cancellationToken = default);
+        Task RollBackTransactionAsync(CancellationToken cancellationToken = default);
+    }
+}
