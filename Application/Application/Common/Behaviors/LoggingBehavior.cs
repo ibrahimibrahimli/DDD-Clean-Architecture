bc293bc5d7572e4ce7fb9cd21b6266894530606e
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-12-06 09:40:42 +0400

    Add LoggingBehavior to track request execution time and errors
    Implemented a MediatR pipeline behavior that logs the start and end of each
    request, including execution duration and detailed error information when
    exceptions occur. This improves observability and helps diagnose performance
    bottlenecks and failures across the application.

---
+++ Application/Application/Common/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace Application.Common.Behaviors
+{
+    public sealed class LoggingBehavior<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+        public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            var stopwatch = Stopwatch.StartNew();
+
+            _logger.LogInformation($"Handling {requestName} - Start");
+
+            try
+            {
+                var response = await next();
+
+                stopwatch.Stop();
+
+                _logger.LogInformation($"Handled {requestName} - success - Elapsed: {stopwatch.ElapsedMilliseconds}ms");
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+
+                stopwatch.Stop();
+
+                _logger.LogError(ex, $"Handled {requestName} - Failed - Elapsed: {stopwatch.ElapsedMilliseconds}ms");
+
+                throw;
+            }
+        }
+    }
+}
