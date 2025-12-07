e00981f2918f255db16e7536fedb072c3e88d86b
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-12-07 10:11:21 +0400

    Add PerformanceBehavior for MediatR pipeline monitoring
    Introduced the `PerformanceBehavior<Trequest, TResponse>` class to measure and log the execution time of MediatR pipeline requests.
    
    - Implements `IPipelineBehavior` to monitor request performance.
    - Logs a warning for requests exceeding 500ms execution time.
    - Uses `ILogger` for logging and `Stopwatch` for timing.
    - Helps identify and address performance bottlenecks.
    - Added necessary `using` directives and organized the class under the `Application.Common.Behaviors` namespace.

---
+++ Application/Application/Common/Behaviors/PerformanceBehavior.cs
@@ -0,0 +1,40 @@
+﻿using Castle.Core.Logging;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace Application.Common.Behaviors
+{
+    public sealed class PerformanceBehavior<Trequest, TResponse>
+        : IPipelineBehavior<Trequest, TResponse>
+        where Trequest : IRequest<TResponse>
+    {
+        readonly ILogger<PerformanceBehavior<Trequest, TResponse>> _logger;
+        readonly Stopwatch _timer;
+
+        public PerformanceBehavior(ILogger<PerformanceBehavior<Trequest, TResponse>> logger)
+        {
+            _logger = logger;
+            _timer = new Stopwatch();
+        }
+        public async Task<TResponse> Handle(Trequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            _timer.Start();
+
+            var response = await next();
+
+            _timer.Stop();
+
+            var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+
+            if (elapsedMilliseconds > 500)
+            {
+                var requestName = typeof(Trequest).Name;
+
+                _logger.LogWarning($"Long running request: {requestName} - {elapsedMilliseconds} milliseconds")
+            }
+
+            return response;
+        }
+    }
+}
