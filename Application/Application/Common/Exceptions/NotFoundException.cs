90e2846c0d25f0cfee46ec62e11a4454e65e41ea
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-12-10 22:47:27 +0400

    Add custom exceptions and error handling utilities
    Added a `NotFoundException` class to handle "not found" scenarios with constructors for entity name/key and custom messages.
    
    Added a `ValidationException` class to handle validation failures, including support for organizing validation errors into a dictionary.
    
    Introduced an `Error` record to centralize error definitions with static predefined errors and a nested `Product` class for product-specific error handling.
    
    These changes improve error handling, validation reporting, and consistency across the application.

---
+++ Application/Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Common.Exceptions
+{
+    public sealed class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key) : base($"Entity \"{name}\" ({key}) was not found"){}
+        public NotFoundException(string messasge) :  base(messasge) { }
+    }
+}
---
+++ Application/Application/Common/Models/Error.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.Common.Models
+{
+    public sealed record Error(string Code, string Message)
+    {
+        static readonly Error None = new (string.Empty, string.Empty);
+        static readonly Error NullValue = new("Error.NullValue", "Null value provided");
+
+
+        public static class Product
+        {
+            public static Error NotFound(Guid id) => new("Product.NotFound", $"Product with ID '{id}' was not found");
+            public static Error InvalidName => new("Product.InvalidName", "Product name is invalid");
+            public static Error InvalidPrice => new("Product.InvalidPrice", "Product price must be positive");
+            public static Error InvalidStock => new("Product.InvalidStock", "Stock quantity cannot be negative");
+            public static Error AlreadyDeleted => new("Product.AlreadyDeleted", "Product is already deleted");
+            public static Error SkuAlreadyExists(string sku) => new("Product.SkuExists", $"Product with SKU '{sku}' already exists");
+            public static Error InsufficientStock => new("Product.InsufficientStock", "Insufficient stock quantity");
+        }
+    }
+}
