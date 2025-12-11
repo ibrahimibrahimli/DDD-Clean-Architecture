4d97c8e72f0bebe0f4330f2d96dcbaba915b5da9
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-12-11 23:09:53 +0400

    Add Persistance project and comment out Product query handler
    Added new Persistance project with EF Core and ApplicationDbcontext.
    Updated solution file to include Persistance project.

--- Application/Application/Features/Products/Queries/GetProductById/GetProductByIdQueryHandler.cs
+++ Application/Application/Features/Products/Queries/GetProductById/GetProductByIdQueryHandler.cs
@@ -5,18 +5,18 @@ using MediatR;
 
 namespace Application.Features.Products.Queries.GetProductById
 {
-    public sealed class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Result<ProductDto>>
-    {
-        readonly IUnitOfWork _unitOfWork;
+    //public sealed class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Result<ProductDto>>
+    //{
+    //    readonly IUnitOfWork _unitOfWork;
 
-        public GetProductByIdQueryHandler(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
+    //    public GetProductByIdQueryHandler(IUnitOfWork unitOfWork)
+    //    {
+    //        _unitOfWork = unitOfWork;
+    //    }
 
-        public async Task<Result<ProductDto>> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
-        {
-            //var product = await _unitOfWork.Products
-        }
-    }
+    //    public async Task<Result<ProductDto>> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+    //    {
+    //        //var product = await _unitOfWork.
+    //    }
+    //}
 }
---
+++ Infrustructure/Persistance/ApplicationDbcontext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance
+{
+    public sealed class ApplicationDbcontext : DbContext
+    {
+        public ApplicationDbcontext(DbContextOptions<ApplicationDbcontext> options) : base(options) { }
+        
+        public DbSet<Product> Products => Set<Product>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbcontext).Assembly);
+        }
+    }
+}
