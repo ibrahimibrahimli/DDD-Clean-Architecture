3459549559e56075a03b786491a48bf2ce739c24
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-12-12 22:45:17 +0400

    Refactor DbContext, add UnitOfWork scaffold, update refs
    Refactored ApplicationDbcontext into Persistance.Context, added domain event handling in SaveChangesAsync, and updated csproj to reference Application project. Introduced UnitOfWork class implementing IUnitOfWork as a scaffold for transaction management.

---
+++ Infrustructure/Persistance/Context/ApplicationDbcontext.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Common;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance.Context
+{
+    public sealed class ApplicationDbcontext : DbContext
+    {
+        public ApplicationDbcontext(DbContextOptions<ApplicationDbcontext> options) : base(options) { }
+
+        public DbSet<Product> Products => Set<Product>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbcontext).Assembly);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var domainEvents = ChangeTracker.Entries<BaseEntity>()
+           .Select(e => e.Entity)
+           .Where(e => e.DomainEvents.Any())
+           .SelectMany(e => e.DomainEvents)
+           .ToList();
+
+            var result = await base.SaveChangesAsync(cancellationToken);
+
+            // (burada event dispatcher istifadə edilə bilər)
+            // MediatR ilə publish edə biləriy - bumu gələcəkdə əlavə edəcəyiy
+            foreach (var entity in ChangeTracker.Entries<BaseEntity>().Select(e => e.Entity))
+            {
+                entity.ClearDomainEvents();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Infrustructure/Persistance/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using Application.Common.Interfaces;
+
+namespace Persistance.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        public Task BeginTransactionAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CommitTransactionAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RollBackTransactionAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
