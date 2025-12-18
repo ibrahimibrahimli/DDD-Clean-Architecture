f126829556a7d279ddfa5ec79699ab29b4bbac75
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-12-18 20:31:46 +0100

    Update EF Core packages and add Product entity config
    Removed legacy EntityFramework, added EF Core Relational and SqlServer packages. Introduced ProductConfiguration for detailed Product entity mapping, including value objects, constraints, indexes, and query filters.

---
+++ Infrustructure/Persistance/Configurations/ProductConfiguration.cs
@@ -0,0 +1,77 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistance.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+
+            builder.Property(p => p.Id)
+                .ValueGeneratedNever();
+
+            builder.HasKey(P => P.Id);
+
+            builder.OwnsOne(p => p.Name, name =>
+            {
+                name.Property(n => n.Value)
+                    .HasColumnName("Name")
+                    .HasMaxLength(200)
+                    .IsRequired();
+            });
+
+            // Money Value Object
+            builder.OwnsOne(p => p.Price, price =>
+            {
+                price.Property(m => m.Amount)
+                    .HasColumnName("Price")
+                    .HasPrecision(18, 2)
+                    .IsRequired();
+
+                price.Property(m => m.Currency)
+                    .HasColumnName("Currency")
+                    .HasMaxLength(3)
+                    .IsRequired();
+            });
+
+            builder.Property(p => p.Description)
+           .HasMaxLength(1000)
+           .IsRequired();
+
+            builder.Property(p => p.StockQuantity)
+                .IsRequired();
+
+            builder.Property(p => p.Sku)
+                .HasMaxLength(50);
+
+            builder.Property(p => p.IsActive)
+                .IsRequired()
+                .HasDefaultValue(true);
+
+            builder.Property(p => p.IsDeleted)
+                .IsRequired()
+                .HasDefaultValue(false);
+
+            builder.Property(p => p.CreatedAt)
+                .IsRequired();
+
+            builder.Property(p => p.UpdatedAt);
+
+            builder.Property(p => p.UpdatedAt);
+
+            builder.HasIndex(p => p.Sku)
+                .IsUnique()
+                .HasFilter("[Sku] is not null and [IsDeleted] = 0");
+
+            builder.HasIndex(p => p.IsDeleted);
+            builder.HasIndex(p => p.IsActive);
+
+            builder.HasQueryFilter(p => !p.IsDeleted);
+
+            builder.Ignore(p => p.DomainEvents);
+        }
+    }
+}
