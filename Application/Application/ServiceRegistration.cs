b917e7ca43b8b5f4166b7316b90f9405a03e3016
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-12-19 20:53:22 +0100

    Add DI registration for MediatR, FluentValidation, behaviors
    Added FluentValidation.DependencyInjectionExtensions to enable DI-based validator registration. Introduced ServiceRegistration.cs with AddAplication extension method to register MediatR handlers, validators, and pipeline behaviors (Logging, Validation, Performance, UnitOfWork) from the current assembly. Streamlines application service setup for dependency injection.

---
+++ Application/Application/ServiceRegistration.cs
@@ -0,0 +1,41 @@
+﻿using Application.Common.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddAplication(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(assembly);
+            });
+                
+            services.AddValidatorsFromAssembly(assembly);
+
+            services.AddTransient(
+                typeof(IPipelineBehavior<,>),
+                typeof(LoggingBehavior<,>));
+
+            services.AddTransient(
+               typeof(IPipelineBehavior<,>),
+               typeof(ValidationBehavior<,>));
+
+            services.AddTransient(
+               typeof(IPipelineBehavior<,>),
+               typeof(PerformanceBehavior<,>));
+
+            services.AddTransient(
+               typeof(IPipelineBehavior<,>),
+               typeof(UnitOfWorkBehavior<,>));
+
+            return services;
+        }
+    }
+}
