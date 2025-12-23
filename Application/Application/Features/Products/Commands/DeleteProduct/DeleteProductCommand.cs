a2d7ace8d239c38e50e7aaf899d2adc435b319d5
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-12-23 22:10:21 +0100

    Add DeleteProduct command and handler with CQRS pattern
    Introduced DeleteProductCommand and DeleteProductCommandHandler
    using MediatR and CQRS. The handler checks for product existence
    and deletion status, updates the product as deleted, and returns
    appropriate Result outcomes. Utilizes IUnitOfWork for data access.

---
+++ Application/Application/Features/Products/Commands/DeleteProduct/DeleteProductCommand.cs
@@ -0,0 +1,8 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.Features.Products.Commands.DeleteProduct
+{
+    public sealed record DeleteProductCommand(Guid Id) : IRequest<Result>;
+    
+}
---
+++ Application/Application/Features/Products/Commands/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+
+namespace Application.Features.Products.Commands.DeleteProduct
+{
+    public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand, Result>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteProductCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _unitOfWork.Products.GetByIdAsync(request.Id);
+
+            if(product is null)
+                return Result.Failure(Error.Product.NotFound(request.Id).Message);
+            
+
+            if(product.IsDeleted)
+                return Result.Failure(Error.Product.AlreadyDeleted.Message);
+
+            product.Delete();
+            await _unitOfWork.Products.UpdateAsync(product, cancellationToken);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Success();
+
+        }
+    }
+}
