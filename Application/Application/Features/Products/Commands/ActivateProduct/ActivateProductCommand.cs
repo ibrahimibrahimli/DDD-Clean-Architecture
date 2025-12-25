e7ba2ea546067d8278f142decee06b72b16a0f7e
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-12-25 20:52:55 +0100

    Add ActivateProduct command and handler for products
    Introduces ActivateProductCommand and its handler to enable activating or deactivating products by ID. The handler checks for product existence, updates activation status, and persists changes, returning appropriate success or failure results.

---
+++ Application/Application/Features/Products/Commands/ActivateProduct/ActivateProductCommand.cs
@@ -0,0 +1,7 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.Features.Products.Commands.ActivateProduct
+{
+    public sealed record ActivateProductCommand(Guid Id, bool Activate) : IRequest<Result>;
+}
---
+++ Application/Application/Features/Products/Commands/ActivateProduct/ActivateProductCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+
+namespace Application.Features.Products.Commands.ActivateProduct
+{
+    public sealed class ActivateProductCommandHandler : IRequestHandler<ActivateProductCommand, Result>
+    {
+        readonly IUnitOfWork _unitOfWork;
+
+        public ActivateProductCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(ActivateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _unitOfWork.Products.GetByIdAsync(request.Id, cancellationToken);
+
+            if (product is null)
+            {
+                return Result.Failure(Error.Product.NotFound(request.Id).Message);
+            }
+
+            if (request.Activate)
+                product.Activate();
+            else
+                product.Deactivate();
+
+            await _unitOfWork.Products.UpdateAsync(product, cancellationToken);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Success();
+        }
+    }
+}
