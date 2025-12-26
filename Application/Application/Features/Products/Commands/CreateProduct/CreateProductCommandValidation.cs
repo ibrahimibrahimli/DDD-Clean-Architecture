f6fd6e952cf08d752b902a9aa954c41ac75a0ab9
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-12-26 23:24:50 +0100

    CreateProductCommandValidation created

---
+++ Application/Application/Features/Products/Commands/CreateProduct/CreateProductCommandValidation.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Products.Commands.CreateProduct
+{
+    public class CreateProductCommandValidation : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidation()
+        {
+            RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Product name is required")
+            .MinimumLength(3).WithMessage("Product name must be at least 3 characters")
+            .MaximumLength(200).WithMessage("Product name cannot exceed 200 characters");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Product description is required")
+                .MaximumLength(1000).WithMessage("Description cannot exceed 1000 characters");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than 0");
+
+            RuleFor(x => x.Currency)
+                .NotEmpty().WithMessage("Currency is required")
+                .Length(3).WithMessage("Currency must be 3 characters (e.g., USD, EUR)")
+                .Matches("^[A-Z]{3}$").WithMessage("Currency must be in uppercase (e.g., USD)");
+
+            RuleFor(x => x.StockQuantity)
+                .GreaterThanOrEqualTo(0).WithMessage("Stock quantity cannot be negative");
+
+            RuleFor(x => x.Sku)
+                .MaximumLength(50).WithMessage("SKU cannot exceed 50 characters")
+                .When(x => !string.IsNullOrWhiteSpace(x.Sku));
+        }
+    }
+}
